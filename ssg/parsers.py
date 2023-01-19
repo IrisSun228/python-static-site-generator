2b3a711d40f5d9eb9ea9fe1d1ff5e710c2db6ab9
Iris Sun <irissun@live.com>
2023-01-19 18:34:27 -0500

    Sec. 4 Step 2 - Markdown Parser

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -36,3 +36,6 @@ class ResourceParser(Parser):
 
     def parse(self, path, source, dest):
         self.copy(path, source, dest)
+
+class MarkdownParser(Parser):
+    extensions = [".md", ".markdown"]
